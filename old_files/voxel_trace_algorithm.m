6a267b6fb267d49c9eb1894bb2b12e066d1ebf1b
Mikael <Mikael@130.235.245.80>
2019-11-07 15:43:42 +0100

    Uppdate on retina trace function.
    Old testcode for voxel trace is moved to old files

--- old_files/voxel_trace_algorithm.m
+++ old_files/voxel_trace_algorithm.m
@@ -1,23 +1,3 @@
-% add subfolders
-% addpath(genpath(pwd))
-
-% figure(1)
-% hold on
-% axis equal
-% scatter3(fitted_points(:,1),fitted_points(:,2),fitted_points(:,3),'.b')
-% scatter3(points(:,1),points(:,2),points(:,3),'.r')
-% scatter3(oP(:,1),oP(:,2),oP(:,3),'.c')
-% scatter3(mP(:,1),mP(:,2),mP(:,3),'.b')
-% scatter3(aP(:,1),aP(:,2),aP(:,3),'+g')
-
-% scatter3(oP(i,1),oP(i,2),oP(i,3),'k')
-% scatter3(mP(i,1),mP(i,2),mP(i,3),'.b')
-% scatter3(aP(i,1),aP(i,2),aP(i,3),'k')
-% quiver3(aP(i,1),aP(i,2),aP(i,3),cone_dir(i,1),cone_dir(i,2),cone_dir(i,3),8)
-% quiver3(aP(i,1),aP(i,2),aP(i,3),pvec(1),pvec(2),pvec(3),8)
-% % % quiver3(mP(:,1),mP(:,2),mP(:,3),cone_dir(:,1),cone_dir(:,2),cone_dir(:,3),4)
-% scatter3(points(p_ind,1),points(p_ind,2),points(p_ind,3),'r')
-
 close all
 clc
 
---
+++ trace_retina.m
@@ -0,0 +1,143 @@
+function absorption_values = trace_retina(receptor_volume,source, abs_coeff, volume_id)
+% Function for tracing the absorption in volume with volume_id. 
+
+% ! This function assumes that each ray only passes through a absorption
+% volume once !
+
+% Determine the bounding volume of the receptor grid.
+boundingBox = [(receptor_volume.boxMin-receptor_volume.stepSize./2) (receptor_volume.boxMax+receptor_volume.stepSize./2)];
+boxSize = [boundingBox(4)-boundingBox(1),boundingBox(5)-boundingBox(2),boundingBox(6)-boundingBox(3)];
+boxN = round(((receptor_volume.boxMax-receptor_volume.boxMin)+receptor_volume.stepSize)./receptor_volume.stepSize);
+
+% Create a zero array for the absorption values of the receptors
+absorption_values = zeros(receptor_volume.receptor_nums,1);
+
+% Trace the ray absorption for all rays in parallel
+arrayfun(@voxel_trace,1:source.num_rays);
+
+function voxel_trace(ray_ind)
+    if ~ismember(source.absorption(ray_ind,:),volume_id)
+        return;
+    end
+        
+    start = source.
+    % Determine if ray starts outside or inside the voxel grid
+    if start(1) < boundingBox(1) || start(1) > boundingBox(4)
+        outside = 1;
+    elseif start(2) < boundingBox(2) || start(2) > boundingBox(5)
+        outside = 1;
+    elseif start(3) < boundingBox(3) || start(3) > boundingBox(6)
+        outside = 1;
+    else
+        outside = 0;
+    end
+    
+    % If outside then find first intersection point
+    if outside
+        [tmin, flag] = rayBoxGPU(boundingBox,start, dir);
+        if ~flag
+            return
+        end
+        start = start + tmin*dir;
+    end
+    
+    x = floor(((start(1)-boundingBox(1))/boxSize(1))*boxN(1)) + 1;
+    y = floor(((start(2)-boundingBox(2))/boxSize(2))*boxN(2)) + 1;
+    z = floor(((start(3)-boundingBox(3))/boxSize(3))*boxN(3)) + 1;
+    
+    if x <= 0; x = 1;
+    elseif x > boxN(1); x = boxN(1); end
+    if y <= 0; y = 1;
+    elseif y > boxN(2); y = boxN(2); end
+    if z <= 0; z = 1;
+    elseif z > boxN(3); z = boxN(3); end
+    
+    
+    if (dir(1)>=0)
+        tVoxelX = (x)/boxN(1);
+        stepX = 1;
+    else
+        tVoxelX = (x-1)/boxN(1);
+        stepX = -1;
+    end
+    if (dir(2)>=0)
+        tVoxelY = (y)/boxN(2);
+        stepY = 1;
+    else
+        tVoxelY = (y-1)/boxN(2);
+        stepY = -1;
+    end
+    if (dir(3)>=0)
+        tVoxelZ = (z)/boxN(3);
+        stepZ = 1;
+    else
+        tVoxelZ = (z-1)/boxN(3);
+        stepZ = -1;
+    end
+    
+    voxelMaxX  = boundingBox(1) + tVoxelX*boxSize(1);
+    voxelMaxY  = boundingBox(2) + tVoxelY*boxSize(2);
+    voxelMaxZ  = boundingBox(3) + tVoxelZ*boxSize(3);
+    
+    if dir(1) == 0
+        tMaxX = inf;
+    else
+        tMaxX = tmin + (voxelMaxX-start(1))/dir(1);
+    end
+    if dir(2) == 0
+        tMaxY = inf;
+    else
+        tMaxY = tmin + (voxelMaxY-start(2))/dir(2);
+    end
+    if dir(3) == 0
+        tMaxZ = inf;
+    else
+        tMaxZ = tmin + (voxelMaxZ-start(3))/dir(3);
+    end
+    
+    voxelSizeX = boxSize(1)/boxN(1);
+    voxelSizeY = boxSize(2)/boxN(2);
+    voxelSizeZ = boxSize(3)/boxN(3);
+    
+    tDeltaX = voxelSizeX/abs(dir(1));
+    tDeltaY = voxelSizeY/abs(dir(2));
+    tDeltaZ = voxelSizeZ/abs(dir(3));
+    
+    maxStep = (max(boxN))^2;
+    step = 1;
+    I = 1;
+    while ( (x<=boxN(1)&&(x>=1)) && (y<=boxN(2))&&(y>=1) && (z<=boxN(3))&&(z>=1) )
+        %     path(step,1) = x;
+        %     path(step,2) = y;
+        %     path(step,3) = z;
+        if point_inds(x,y,z) ~= 0
+            In = exp(-step*abs_coeff);
+            absVal = I - In;
+            I = In;
+            receptors(nonzeros(receptors_inds(point_inds(x,y,z),:))) = receptors(nonzeros(receptors_inds(point_inds(x,y,z),:))) + absVal;
+            step = step + 1;
+        end
+        
+        if (tMaxX < tMaxY)
+            if (tMaxX < tMaxZ)
+                x = x + stepX;
+                tMaxX = tMaxX + tDeltaX;
+            else
+                z = z + stepZ;
+                tMaxZ = tMaxZ + tDeltaZ;
+            end
+        else
+            if (tMaxY < tMaxZ)
+                y = y + stepY;
+                tMaxY = tMaxY + tDeltaY;
+            else
+                z = z + stepZ;
+                tMaxZ = tMaxZ + tDeltaZ;
+            end
+        end
+        %     step = step+1;
+    end
+    % path = path(1:step-1,:);
+    
+end
+end
